86efeb652e4c2636203df1ad197ee3f4253baed5
unknown <20176022>
2017-08-25 14:43:41 +0530

    lcs added

---
+++ Bag_of_words.py
@@ -0,0 +1,98 @@
+import os
+import string
+import math
+import re
+
+
+class plagarism():
+    def __init__(self):
+        self
+    def rep(self,t):
+        """
+        This function is used for replacing
+        the special characters in the string
+        """
+        return re.sub('[^A-Za-z0-9\_]+', ' ',t)
+
+    def frequency(self,d,dict1):
+        """
+        This function is used for calculating
+        the frequency of the file
+        """
+        for i in d:
+            if i==" " or i=="" or i=="\n":
+                pass
+            elif i not in dict1:
+                dict1[i]=1
+            else:
+                dict1[i]+=1
+        return dict1
+
+    def dotp(self,d1,d2):
+        """
+        This function is used for calculating
+        the dot product of the files
+        """
+        L=[]
+        for i in d1:
+            for j in d2:
+                if i==j:
+                    L.append(d1[i]*d2[j])
+        return sum(L)
+
+    def modfreq(self,d):
+        """
+        This function is used for calculating
+        the modulus frequency of the file
+        """
+        s=0
+        for i in d:
+            s+=(d[i])**2
+        p=s**(1/2)
+        return p
+    def plag(self):
+        """
+        This function is used for calculating
+        the plagarism of the files
+        """
+        path=input("Enter path:")
+        a=os.listdir(path)
+        os.chdir(path)
+        print(a)
+        for k in a:
+            file1=open(k,"r")
+            y=[]
+            t1=file1.read()
+            t1=t1.lower()
+            #t1.replace("\n"," ")
+            t1=self.rep(t1)
+            t1=t1.split(" ")
+            #t1=[z.strip(string.punctuation) for z in t1]
+            dict1={}
+            dict1=self.frequency(t1,dict1)
+            s1=self.modfreq(dict1)
+            for j in a:
+                if k==j:
+                    y.append(None)
+                else:
+                    file2=open(j,"r")
+                    t2=file2.read()
+                    t2=t2.lower()
+                    t2=self.rep(t2)
+                    t2=t2.split(" ")
+                    dict2={}
+                    dict2=self.frequency(t2,dict2)
+                    freq=self.dotp(dict1,dict2)
+                    s2=self.modfreq(dict2)
+                    tsum=s1*s2
+                    y.append(str(round((freq/tsum)*100))+"%")
+                    file2.close()
+            print(k,y)
+            print("\n")
+            file1.close()
+
+"""
+The main program execution starts here
+"""
+p=plagarism()
+p.plag()
---
+++ lcs.py
@@ -0,0 +1,85 @@
+import os
+import string
+import math
+import re
+class gouse(Exception):
+    pass
+    
+class plagarism():
+    def __init__(self):
+        self
+#methods
+    def subs(self,f1,f2):
+        s=0
+        for i in range(len(f1)):
+            itemp=i
+            item=itemp
+            for j in range(len(f2)):
+                if (itemp<len(f1)):
+                    if f1[itemp]==f2[j]:
+                        itemp+=1
+                        if (itemp-i)>s:
+                            s=itemp-i
+                            lcs=f1[i:itemp]
+                    else:
+                        itemp=item
+        return lcs
+    def rep(self,t):
+        """
+        This function is used for replacing
+        the special characters in the string
+        """
+        t=re.sub("[^a-zA-Z0-9\_\n]+","",t)
+        return t
+
+    def plag(self):
+        """
+        This function is used for calculating
+        the plagarism of the files
+        """
+        path=input("Enter path:")
+        a=os.listdir(path)
+        os.chdir(path)
+        print("     ",a)
+        for k in a:
+            file1=open(k,"r")
+            y=[]
+            z=[]
+            t1=file1.read()
+            try:
+                if t1=="":
+                    raise gouse
+                else:
+                    t1=t1.lower()
+                    t1=self.rep(t1)
+                    for j in a:
+                        if k==j:
+                            y.append(None)
+                            z.append(None)
+                        else:
+                            file2=open(j,"r")
+                            t2=file2.read()
+                            try:
+                                if t2=="":
+                                    raise gouse
+                                else:
+                                    t2=t2.lower()
+                                    t2=self.rep(t2)
+                                    ss=self.subs(t1,t2)
+                                    z.append(ss)
+                                    dub=len(ss)
+                                    y.append(round((((dub*2)/(len(t1)+len(t2)))*100)))
+                                    file2.close()
+                            except gouse:
+                                y.append("File Empty")
+            except gouse:
+                y.append("File Empty")
+            print(k,y)
+            print("\n")
+            print("     ",z)
+            file1.close()
+
+p=plagarism()
+p.plag()
+            
+
